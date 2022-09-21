59ee6b31d0ed6909d2925cf1f2e44a5cbcd5ef32
Emediong Benison <emediongbenison@yahoo.com>
2022-09-21 18:11:32 +0100

    menu toggle added

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
-$(document).ready(function() {
+// show/hide nav menu
+const menu = document.querySelector('.navList');
+const closeMenu = document.querySelector('#close-menu-btn');
+const openMenu = document.querySelector('#open-menu-btn');
 
-    $('.menu-toggler').on('click', function() {
-        $(this).toggleClass('open');
-        $('.top-nav').toggleClass('open');
-    });
+openMenu.addEventListener('click', toggleMenu);
+closeMenu.addEventListener('click', closeToggleMenu);
 
-    $('.top-nav .nav-link').on('click', function() {
-        $('.menu-toggler').removeClass('open');
-        $('.top-nav').removeClass('open');
-    });
 
-    const up = document.querySelector(".up");
-    up.addEventListener('click', function() {
-        $('html, body').animate({ scrollTop: 500 }, 3000);
-    });
+function toggleMenu () {
+    menu.style.display = 'block';
+    openMenu.style.display = 'none';
+    closeMenu.style.display = 'block';
+}
 
-    
-    /*
-    $('nav a[href*="#"]').on('click', function() {
-        $('html, body').animate(keyframes: {
-            scrollTop: $($(this).attr('href')).offset().top - 100
-        }, options: 2000);
-    });
-    
-    $('#up').on('click', function() {
-        $('html, body').animate(keyframes: {
-            scrollTop: 0
-        }, options: 2000);
-    });
-*/
-});
+function closeToggleMenu () {
+    menu.style.display = 'none';
+    openMenu.style.display = 'block';
+    closeMenu.style.display = 'none';
+}
